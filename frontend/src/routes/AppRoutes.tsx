a98329e719f5b40977a2dfe63b7607ea709e5c6e
Do Quoc Tien <hadesdqt@gmail.com>
2026-04-22 09:57:47 +0700

    feat: add auth layout and routes (login, signup, forgot, reset password)

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,4 +1,9 @@
+import AuthLayout from "@/layouts/AuthLayout";
 import MainLayout from "@/layouts/MainLayout";
+import ForgotPassword from "@pages/auth/ForgotPassword";
+import Login from "@pages/auth/Login";
+import ResetPassword from "@pages/auth/ResetPassword";
+import Signup from "@pages/auth/Signup";
 import ForbiddenPage from "@pages/error/ForbiddenPage";
 import NotFoundPage from "@pages/error/NotFoundPage";
 import Home from "@pages/home/Home";
@@ -10,6 +15,14 @@ const AppRoutes = () => {
       {/* Layout Website */}
       <Route element={<MainLayout />}>
         <Route path="/" element={<Home />} />
+      </Route>
+
+      {/* Auth layout */}
+      <Route element={<AuthLayout />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password/:token" element={<ResetPassword />} />
       </Route>
 
       {/* Error pages */}
