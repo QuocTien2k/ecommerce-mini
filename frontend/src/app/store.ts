5ff6fdf33bc4dc5b1184ab96831c447e1bac913f
Do Quoc Tien <hadesdqt@gmail.com>
2026-04-30 10:19:57 +0700

    refactor(redux): remove loading.slice and cleanup store

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "@/features/auth/auth.slice";
-import loadingReducer from "@/features/loading/loading.slice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    loading: loadingReducer,
   },
 });
 
