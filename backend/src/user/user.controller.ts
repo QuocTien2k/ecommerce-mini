f336fb1efed65d323c16d51b24d9e0424f189fff
Do Quoc Tien <hadesdqt@gmail.com>
2026-05-01 15:19:44 +0700

    refactor(user): use @CurrentUser('sub') instead of @Req to extract userId

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -3,9 +3,6 @@ import {
   Controller,
   Get,
   Patch,
-  Post,
-  Req,
-  Request,
   UploadedFile,
   UseGuards,
   UseInterceptors,
@@ -18,37 +15,46 @@ import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { UpdateProfileDto } from './dtos/update-profile.dto';
 import { ChangePasswordDto } from './dtos/change-password.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { CurrentUser } from '@auth/decorators/current-user.decorator';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get('me')
-  @UseGuards(JwtAuthGuard, RolesGuard)
-  @Roles(Role.USER)
-  getMe(@Request() req) {
-    return this.userService.getMe(req.user.sub);
+  @UseGuards(JwtAuthGuard)
+  getMe(@CurrentUser('sub') userId: string) {
+    return this.userService.getMe(userId);
   }
 
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.USER)
   @Patch('profile')
-  async updateProfile(@Req() req, @Body() body: UpdateProfileDto) {
-    return this.userService.updateProfile(req.user.sub, body);
+  async updateProfile(
+    @CurrentUser('sub') userId: string,
+    @Body() body: UpdateProfileDto,
+  ) {
+    return this.userService.updateProfile(userId, body);
   }
 
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.USER)
   @Patch('password')
-  async updatePassword(@Req() req, @Body() body: ChangePasswordDto) {
-    return this.userService.updatePassword(req.user.sub, body);
+  async updatePassword(
+    @CurrentUser('sub') userId: string,
+    @Body() body: ChangePasswordDto,
+  ) {
+    return this.userService.updatePassword(userId, body);
   }
 
   @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(Role.USER)
   @Patch('avatar')
   @UseInterceptors(FileInterceptor('file'))
-  async uploadAvatar(@Req() req, @UploadedFile() file: Express.Multer.File) {
-    return this.userService.uploadAvatar(req.user.sub, file);
+  async uploadAvatar(
+    @CurrentUser('sub') userId: string,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    return this.userService.uploadAvatar(userId, file);
   }
 }
