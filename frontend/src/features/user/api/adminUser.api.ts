a11e667c25dafb22bcd5c91036e48ea277b962b3
Do Quoc Tien <hadesdqt@gmail.com>
2026-05-08 09:59:47 +0700

    feat(frontend-admin/users): implement debounced user filtering

--- frontend/src/features/user/api/adminUser.api.ts
+++ frontend/src/features/user/api/adminUser.api.ts
@@ -1,9 +1,12 @@
 import { api } from "@shared/api/axios";
 import type { AdminUser, GetUsersParams } from "../types/adminUser.type";
 import type { ApiResult } from "@shared/types/api-result";
+import type { PaginatedResponse } from "@shared/types/pagination";
 
 export const adminUserApi = {
-  getUsers: (params?: GetUsersParams): ApiResult<AdminUser> =>
+  getUsers: (
+    params?: GetUsersParams,
+  ): ApiResult<PaginatedResponse<AdminUser>> =>
     api.get("/admin/users", { params }),
 
   lockUser: (userId: string): ApiResult<null> =>
--- frontend/src/features/user/hooks/useAdminUsersQuery.ts
+++ frontend/src/features/user/hooks/useAdminUsersQuery.ts
@@ -1,19 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
-import { adminUserApi, type GetUsersParams } from "../api/adminUser.api";
-import type { AdminUser } from "../types/adminUser.type";
-import type { ApiResponse } from "@shared/types/api-response";
-import type { PaginatedResponse } from "@shared/types/pagination";
+import { adminUserApi } from "../api/adminUser.api";
+import type { GetUsersParams } from "../types/adminUser.type";
 
 export const ADMIN_USERS_QUERY_KEY = "admin-users";
 
 export const useAdminUsersQuery = (params?: GetUsersParams) => {
   return useQuery({
     queryKey: [ADMIN_USERS_QUERY_KEY, params],
-    queryFn: async (): Promise<ApiResponse<PaginatedResponse<AdminUser>>> => {
-      await new Promise((r) => setTimeout(r, 1000));
-      const res = await adminUserApi.getUsers(params);
-      return res;
-    },
+
+    queryFn: () => adminUserApi.getUsers(params),
+
     placeholderData: (prev) => prev,
   });
 };
