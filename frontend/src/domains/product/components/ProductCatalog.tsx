cdd7903de5a116bfa22af55fb1b1ce6b9f9f9094
Do Quoc Tien <hadesdqt@gmail.com>
2026-06-16 16:27:07 +0700

    feat(frontend - domains/product): add breadcrumb display from API with stable state

--- frontend/src/domains/product/components/ProductCatalog.tsx
+++ frontend/src/domains/product/components/ProductCatalog.tsx
@@ -7,8 +7,15 @@ import { ProductNotFound } from "@components/product/ProductNotFound";
 
 import { SectionTitle } from "@components/ui/section-title";
 import { ProductFilters } from "./search/ProductFilter";
+import { useEffect, useState } from "react";
+
+type BreadcrumbItem = {
+  name: string;
+  href?: string;
+};
 
 export const ProductCatalog = () => {
+  const [breadcrumb, setBreadcrumb] = useState<BreadcrumbItem[]>([]);
   const {
     setPage,
 
@@ -23,6 +30,14 @@ export const ProductCatalog = () => {
 
   const products = data?.data.data ?? [];
 
+  //console.log("Data: ", data?.data.breadcrumb);
+
+  useEffect(() => {
+    if (data?.data?.breadcrumb) {
+      setBreadcrumb(data.data.breadcrumb);
+    }
+  }, [data]);
+
   const meta = data?.data.meta;
 
   return (
@@ -34,6 +49,32 @@ export const ProductCatalog = () => {
           description="Danh sách sản phẩm hiện có trong cửa hàng"
         />
       </div>
+
+      {/* Breadcrumb */}
+      <nav className="text-sm text-gray-500 min-h-5">
+        {breadcrumb.length > 0 ? (
+          <ol className="flex items-center gap-2">
+            {breadcrumb.map((item, index) => (
+              <li
+                key={`${item.name}-${index}`}
+                className="flex items-center gap-2"
+              >
+                {index !== 0 && <span>/</span>}
+
+                {item.href ? (
+                  <a href={item.href} className="hover:text-gray-900">
+                    {item.name}
+                  </a>
+                ) : (
+                  <span className="text-gray-700 font-medium">{item.name}</span>
+                )}
+              </li>
+            ))}
+          </ol>
+        ) : (
+          <div className="opacity-0 select-none">breadcrumb</div>
+        )}
+      </nav>
 
       <div className="grid gap-6 lg:grid-cols-12">
         {/* Filter */}
